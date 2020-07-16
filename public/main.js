36945ed80c3a8629f843c1270ba5ae054f1405ac
Soorria Saruva <soorria.ss@gmail.com>
2020-07-16 12:14:33 +1000

    Improve frontend styling

---
+++ public/main.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("form")
+const shortInput = document.getElementById("short-input")
+const urlInput = document.getElementById("url-input")
+const errDiv = document.getElementById("error")
+const sucDiv = document.getElementById("success")
+const resInput = document.getElementById("result")
+const tryLink = document.getElementById("try")
+
+const API_URL = "http://192.168.1.107:3000/"
+
+function showSuccessStuff(isSuccess) {
+  sucDiv.classList[isSuccess ? "remove" : "add"]("hidden")
+  resInput.classList[isSuccess ? "remove" : "add"]("hidden")
+  tryLink.classList[isSuccess ? "remove" : "add"]("hidden")
+  errDiv.classList[!isSuccess ? "remove" : "add"]("hidden")
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault()
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "short": shortInput.value,
+      "url": urlInput.value,
+    }),
+  })
+  const data = await res.json()
+
+  if (data.success) {
+    showSuccessStuff(true)
+    sucDiv.textContent = "shortened"
+    const link = `${API_URL}${data.short}`
+    resInput.value = link
+    tryLink.href = link
+  } else {
+    showSuccessStuff(false)
+    errDiv.textContent = data.message
+  }
+})
+
